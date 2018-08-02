9c34cec6225296c805c0ed2e10c5e0407fe95f20
Aurélien <aurcomin@gmail.com>
2018-08-02 11:15:32 +0200

    syncroAurélien

--- GestionCommercialeUIW/frmConsultClient.cs
+++ GestionCommercialeUIW/frmConsultClient.cs
@@ -12,10 +12,47 @@ namespace GestionCommercialeUIW
 {
     public partial class frmConsultClient : Form
     {
-        public frmConsultClient()
+        private GestionCommercialeDll.Client leClient;
+
+        public frmConsultClient(GestionCommercialeDll.Client unClient)
         {
+            this.leClient = unClient;
             InitializeComponent();
+        }
+
+        private void txtNumClient_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void frmConsultClient_Load(object sender, EventArgs e)
+        {
+            this.afficheClient(this.leClient);
+                
+        }
+        private void afficheClient(GestionCommercialeDll.Client unClient)
+        {
+            this.txtNumClient.Text = unClient.NumClient;
+            this.txtRSocial.Text = unClient.RaisonSociale;
+            this.rue.Text = unClient.Adresse;
+            this.textAdresse1.Text = unClient.Adresse;
+            this.txtCP.Text = unClient.CP;
+            this.txtVille.Text = unClient.Ville;
+            this.txtTel.Text = unClient.Telephone;
+            this.txtEffectif.Text = unClient.Effectif.ToString();
+            this.txtActivite.Text = unClient.Activite;
+            this.txtCA.Text = unClient.CA.ToString();
+            this.txtNature.Text = unClient.Nature;
+
         }
 
     }
 }
+        
+      
+
+       
+
+        
+
