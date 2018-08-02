84b0f2113af1e305b968d953e41a3bca40d6be69
Cdi <Cdi@DL09>
2018-08-02 14:32:43 +0200

    Nico, modification sur les caractères numériques

--- GestionCommercialeUIW/frmNouveauClient.cs
+++ GestionCommercialeUIW/frmNouveauClient.cs
@@ -32,7 +32,7 @@ namespace GestionCommercialeUIW
             // permet de remplir par défaut la première valeur de l'index afin de ne pas mettre le programme en erreur
             this.cmbBoxActivité.SelectedIndex = 0;
 
-           
+
         }
 
         private void InitNature()
@@ -47,8 +47,8 @@ namespace GestionCommercialeUIW
             // permet de remplir par défaut la première valeur de l'index afin de ne pas mettre le programme en erreur
             this.cmbBoxNature.SelectedIndex = 0;
 
-            
-            
+
+
         }
 
         private void btnContact_Click(object sender, EventArgs e) // Permet d'ouvrir la fenêtre Nouveau contact par le biais du bouton "Contacts" de la fenêtre Nouveau client
@@ -69,13 +69,186 @@ namespace GestionCommercialeUIW
         private void btnOK_Click(object sender, EventArgs e)
         {
 
-            GestionCommercialeDll.Client nouveauClient = new GestionCommercialeDll.Client();
+            
 
-            try
+            /* try
+             {
+
+
+                 // déclenchement des méthodes get/set du Client.cs
+                 nouveauClient.NumClient = txtBoxNumClient.Text;
+                 nouveauClient.RaisonSociale = txtBoxRaisonSocial.Text;
+                 nouveauClient.Adresse = txtBoxAdresse.Text;
+                 nouveauClient.CP = txtBoxCP.Text;
+                 nouveauClient.Ville = txtBoxVille.Text;
+                 nouveauClient.Telephone = txtBoxTelephone.Text;
+                 nouveauClient.Effectif = Int32.Parse(txtBoxEffectif.Text);
+                 nouveauClient.CA = Int32.Parse(txtBoxCA.Text);
+                 nouveauClient.Activite = cmbBoxActivité.Text;
+                 nouveauClient.Nature = cmbBoxNature.Text;
+                 nouveauClient.Prive = chkBoxPrive.Checked;
+
+                 GestionCommercialeDll.Donnees.TabClients.Add(nouveauClient);
+
+                 GestionCommercialeDll.Client.NClient += 1;
+
+                 this.DialogResult = DialogResult.OK;
+
+             }
+             catch (Exception ex)
+             {
+                 nouveauClient = null;
+                 MessageBox.Show("Erreur : \n" + ex.Message, "Raison sociale");
+             }*/
+
+
+            if (this.controle())
             {
+                if (this.instancie())
+                {
+                    // cas général :
+                    // si l'instanciation stagiaire et
+                    // son ajout à la collection est OK :
+                    // - incrémentation compteurs de stagiaires
+                    
+
+                    // - fermeture de la boite de dialogue par validation
+                    this.DialogResult = DialogResult.OK;
+                }
+            }
+
+        }
+
+        private Boolean controle()
+        {
+
+            // contrôler la vraissemblance de tous les champs
+            Boolean code = true; // le code de retour ; OK a priori
+                                 // appel fonction générique de contrôle
+            if (!(estEntier(this.txtBoxNumClient.Text)))
+            {
+                // la chaîne reçue n'est pas convertible
+                code = false;
+                MessageBox.Show("le Numéro client saisi n'est pas un entier valide", "ERREUR", MessageBoxButtons.OK);
+            }
+            if (!(estEntierCP(this.txtBoxCP.Text)))
+            {
+                code = false;
+                MessageBox.Show("le code postal saisi n'est pas correct",
+               "ERREUR", MessageBoxButtons.OK);
+            }
+            if (!(estEntierTelephone(this.txtBoxTelephone.Text)))
+            {
+                // la chaîne reçue n'est pas convertible
+                code = false;
+                MessageBox.Show("le numéro de téléphone saisi n'est pas un entier valide", "ERREUR", MessageBoxButtons.OK);
+            }
+
+            if (!(estEntier(this.txtBoxEffectif.Text)))
+            {
+                // la chaîne reçue n'est pas convertible
+                code = false;
+                MessageBox.Show("l'effectif saisi n'est pas un entier valide", "ERREUR", MessageBoxButtons.OK);
+            }
+            if (!(estEntier(this.txtBoxCA.Text)))
+            {
+                code = false;
+                MessageBox.Show("le chiffre d'affaire saisi n'est pas correct",
+               "ERREUR", MessageBoxButtons.OK);
+            }
+            return code;
+        }
+
+        private Boolean estEntier(String s)
+        {
+            /* vérifier que la chaine reçue représente bien un entier valide :
+            * - uniquement des chiffres
+            * - pas vide
+            * - pas plus de 9 chiffres (capacité maxi du type Int32)
+            */
+            Int32 i; // indice de parcours de chaîne
+            Char c; // caractère courant
+            Boolean code = true; // code retour; OK a priori
+                                 // test longueur chaîne reçue
+            if (s.Length < 10 && s.Length > 0)
+            {
+                // vérifier 1 à 1 que tous les caractères sont des chiffres
+                for (i = 0; i < s.Length; i++)
+                {
+                    c = s[i]; // extrait le i° car
+                    if (!(Char.IsDigit(c))) // si ce n'est pas un chiffre
+                    {
+                        code = false; // erreur détectée
+                    }
+                } // fin de boucle for
+            }
+            else // erreur de longueur de chaine
+            {
+                code = false; // erreur détectée
+            }
+            return code;
+        }
+
+        private Boolean estEntierCP(String s)
+        {
+            
+            Int32 i; // indice de parcours de chaîne
+            Char c; // caractère courant
+            Boolean code = true; // code retour; OK a priori
+                                 // test longueur chaîne reçue
+            if (s.Length == 5)
+            {
+                // vérifier 1 à 1 que tous les caractères sont des chiffres
+                for (i = 0; i < s.Length; i++)
+                {
+                    c = s[i]; // extrait le i° car
+                    if (!(Char.IsDigit(c))) // si ce n'est pas un chiffre
+                    {
+                        code = false; // erreur détectée
+                    }
+                } // fin de boucle for
+            }
+            else // erreur de longueur de chaine
+            {
+                code = false; // erreur détectée
+            }
+            return code;
+        }
 
 
-                // déclenchement des méthodes get/set du Client.cs
+        private Boolean estEntierTelephone(String s)
+        {
+            
+            Int32 i; // indice de parcours de chaîne
+            Char c; // caractère courant
+            Boolean code = true; // code retour; OK a priori
+                                 // test longueur chaîne reçue
+            if (s.,  s.Length == 10)
+            {
+                // vérifier 1 à 1 que tous les caractères sont des chiffres
+                for (i = 0; i < s.Length; i++)
+                {
+                    c = s[i]; // extrait le i° car
+                    if (!(Char.IsDigit(c))) // si ce n'est pas un chiffre
+                    {
+                        code = false; // erreur détectée
+                    }
+                } // fin de boucle for
+            }
+            else // erreur de longueur de chaine
+            {
+                code = false; // erreur détectée
+            }
+            return code;
+        }
+
+
+        private Boolean instancie()
+        {
+            // créer une référence d'objet MStagiaire
+            GestionCommercialeDll.Client nouveauClient = new GestionCommercialeDll.Client();
+            try
+            {// déclenchement des méthodes get/set du Client.cs
                 nouveauClient.NumClient = txtBoxNumClient.Text;
                 nouveauClient.RaisonSociale = txtBoxRaisonSocial.Text;
                 nouveauClient.Adresse = txtBoxAdresse.Text;
@@ -89,18 +262,14 @@ namespace GestionCommercialeUIW
                 nouveauClient.Prive = chkBoxPrive.Checked;
 
                 GestionCommercialeDll.Donnees.TabClients.Add(nouveauClient);
-
-                GestionCommercialeDll.Client.NClient += 1;
-
-                this.DialogResult = DialogResult.OK;
-
+                return true;
             }
             catch (Exception ex)
             {
                 nouveauClient = null;
-                MessageBox.Show("Erreur : \n" + ex.Message, "Raison sociale");
+                MessageBox.Show("Erreur : \n" + ex.Message, "Ajout de Client");
+                return false;
             }
-
 
         }
 
