6a9a101619f4c29091e7e3a18e984ea1f2f61ce6
Alexoyd <alexoyd@hotmail.fr>
2018-08-20 09:12:32 +0200

    Commentaire

--- GestionCommercialeUIW/MDI.Designer.cs
+++ GestionCommercialeUIW/MDI.Designer.cs
@@ -123,7 +123,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(846, 534);
+            this.ClientSize = new System.Drawing.Size(846, 489);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
